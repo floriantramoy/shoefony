8806ee2a3587928653ee45f77d5299978e9bba0b
Tramoy <floriantramoy@gmail.com>
2021-12-04 11:28:16 +0100

    StoreController terminé

--- src/Controller/StoreController.php
+++ src/Controller/StoreController.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types =1);
+
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -9,17 +11,19 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class StoreController extends AbstractController
 {
-    #[Route('/store/product/{id}/details/{slug}', name: 'store_show_product', requirements: ['id'=>'\d+'], methods: ['GET'])]
-    public function product(Request $request, int $id, string $slug): Response
+    #[Route('/store/product/{id}/details/{slug}', name: 'store_show_product', requirements: ['id' => '\d+'], methods: ['GET'])]
+    public function showProduct(Request $request, int $id, string $slug): Response
     {
-        $url = $request->server->get('REQUEST_URI');
-        $ip = $request->getClientIp();
         return $this->render('store/index.html.twig', [
             'controller_name' => 'StoreController',
             'id' => $id,
             'slug' => $slug,
-            'url' => $url,
-            'ip' => $ip
+            'from_routing' => $this->generateUrl('store_show_product', [
+                'id' => $id,
+                'slug' => $slug
+            ]),
+            'ip' => $request->getClientIp(),
+            'url' => $request->getUri()
         ]);
     }
 }
