2bc71ba1d3668c3850ecc355670fa2790d1dcc11
Tramoy <floriantramoy@gmail.com>
2021-12-04 10:47:13 +0100

    Flex remove packages

---
+++ src/Controller/MainController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    #[Route('/', name: 'main_homepage', methods: ['GET'])]
+    public function homepage(): Response
+    {
+        return $this->render('main/index.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+
+    #[Route('/presentation', name: 'main_presentation', methods: ['GET'])]
+    public function presentation(): Response
+    {
+        return $this->render('main/presentation.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+
+    #[Route('/contact', name: 'main_contact', methods: ['GET'])]
+    public function contact(): Response
+    {
+        return $this->render('main/contact.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+}
